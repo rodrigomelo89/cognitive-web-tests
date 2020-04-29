757bb14f5ae137c768ff74c27c0e73a9a09bc29b
Rodrigo Melo <rodrigo.melo89@yahoo.com.br>
2020-04-29 16:01:23 -0300

    first commit, cognitive web tests

---
+++ frontpage/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Exams
+
+admin.site.register(Exams)
---
+++ frontpage/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+class Exams(models.Model):
+    paciente = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    genero = models.CharField(max_length=100)
+    tempo_estudo = models.IntegerField()
+    # experiments =
+    # audio =
+    resultados = models.TextField()
+
+    def examination(self):
+        self.save()
